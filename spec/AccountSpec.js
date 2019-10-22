0b7206fd7138448cdfb824ad994af6710f49fe87
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-10-22 14:25:02 +0100

    Add method getAccountOpeningDate

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -2,13 +2,21 @@
 
 describe('Account', function() {
   var account;
+  var date;
 
   beforeEach(function() {
     account = new Account();
+    date = new Date();
   });
 
   it('should have an initial balance of zero', function() {
     expect(account.getCurrentBalance()).toEqual(0);
+  });
+
+  describe('getDateOpened', function() {
+    it('should return the date the account was opened', function() {
+      expect(account.getDateOpened()).toEqual(date)
+    });
   });
 
 });
--- src/Account.js
+++ src/Account.js
@@ -3,7 +3,13 @@
 function Account() {
   this.INITIAL_BALANCE = 0;
   this.balance = this.INITIAL_BALANCE
+  this.dateOpened = new Date();
 };
+
 Account.prototype.getCurrentBalance = function() {
   return this.balance;
+};
+
+Account.prototype.getDateOpened = function() {
+  return this.dateOpened;
 };
